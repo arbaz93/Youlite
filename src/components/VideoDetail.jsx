9a75871789ac64286e921f6b11c2f440006e1a63
Arbaz yousaf <arbaz.k480@yahoo.com>
2024-09-28 19:26:33 +0500

    "Added a _redirects file in public folder to resolve 404 error when a route page is refreshed."

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -29,7 +29,7 @@ export default function VideoDetail({  }) {
       <Stack direction={{xs: 'column', md: 'row'}}>
         <Box flex='1'>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px'}}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
             <Typography color='#FFF' variant='h5' fontWeight='bold' p={2}>{title}</Typography>
             <Stack direction='row' justifyContent='space-between' sx={{ color: '#FFF'}} py={1} px={2}>
               <Link to={`/channel/${channelId}`}>
