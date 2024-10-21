d1e6a147a52539549e55bb7b4efa705d8ebc80dc
Arbaz Yousaf <arbaz.k480@yahoo.com>
2024-10-21 18:33:25 +0500

    Update Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -4,9 +4,9 @@ export default function Videos({ videos, align, direction }) {
   if(!videos?.length) return '...loading'
 
   return (
-    <Stack direction='column' flexWrap="wrap" justifyContent={align} alignItems="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent={align} alignItems="start" gap={2}>
       {videos.map((item, idx) => (
-        <Box key={idx} width='100%'>
+        <Box key={idx} sx={{width: {xs: '100%', sm: 'auto'}}}>
           {item.id.videoId && <VideoCard video={item} /> }
           {item.id.playlistId && <VideoCard video={item} /> }
           {item.id.channelId && <ChannelCard channelDetail={item} />}
