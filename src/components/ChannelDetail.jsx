af46eb199fe257f9410b4142b836f9083985d347
Arbaz Yousaf <arbaz.k480@yahoo.com>
2024-10-21 18:28:01 +0500

    Update ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -20,8 +20,8 @@ export default function ChannelDetail() {
     <Box minHeight='95vh'>
       <Box style={{ background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)', zIndex: 10, height: '300px'}}></Box>
         <ChannelCard channelDetail={channelDetail} marginTop={'-120px'}/>
-        <Box p='2'>
-          <Box>
+        <Box display='flex' p='2px'>
+          <Box sx={{ display: 'flex'}}>
             <Videos videos={videos} align={'center'} />
           </Box>
         </Box>
