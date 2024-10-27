fff001c86b4562d5b081eb78673926d12242d960
arbaz93 <arbaz.k480@yahoo.com>
2024-10-27 13:38:11 +0500

    "Added a fully functional autoplay toggle button in VideoDetail page with NextVideo.jsx page"

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,26 +1,47 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect, useRef} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import ReactPlayer from 'react-player'
 
 import { Typography, Box, Stack } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
 
-import { Videos } from './'
+import { Videos, NextVideo, ToggleSwitch } from './'
 
 import { fetchFromAPI } from '../utils/fetchFromApi'
 
-export default function VideoDetail({  }) {
+export default function VideoDetail() {
   const [videoDetails, setVideoDetails] = useState(null)
   const [videos, setVideos] = useState(null)
+  const [nextVideos, setNextVideos] = useState(null)
+  const [isVideoEnd, setIsVideoEnd] = useState(false)
   const { id } = useParams()
+  const intervalRef = useRef()
+  let playNextVidRef = useRef(localStorage.getItem('yt-auto'))  
+
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
       .then(data => setVideoDetails(data.items[0]))
     fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
-      .then(data => setVideos(data.items))
+      .then(data => {
+        setVideos(data.items); 
+        setNextVideos(data.items[3])
+      })
   }, [id])
 
+  function playNextVideo() {
+    if (playNextVidRef.current === 'true') {
+      setIsVideoEnd(true)
+      const intervalId = setTimeout(() => {
+        location.href = `http://localhost:5173/video/${nextVideos.id.videoId}`
+      }, 5000)
+      intervalRef.current = intervalId;
+    }
+  }
+  function endInterval() {
+    setIsVideoEnd(false)
+    clearInterval(intervalRef.current)
+  }
   if(!videoDetails?.snippet) return '...loading'
   const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetails;
 
@@ -29,8 +50,9 @@ export default function VideoDetail({  }) {
       <Stack direction={{xs: 'column', md: 'row'}}>
         <Box flex='1'>
           <Box sx={{ width: '100%', position: 'sticky', top: '86px'}}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
-            <Typography color='#FFF' variant='h5' fontWeight='bold' p={2}>{title}</Typography>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} onPlay={() => endInterval()} onEnded={playNextVideo} playing={playNextVidRef.current === 'true' ? true : false} className="react-player" controls/>
+            {isVideoEnd && <NextVideo endInterval={endInterval} nextVid={nextVideos}/>}
+            <Typography display='flex' justifyContent='space-between' color='#FFF' variant='h5' fontWeight='bold' p={2}>{title}<button className='only-text-btn'><ToggleSwitch playNextVidRef={playNextVidRef}/></button></Typography>
             <Stack direction='row' justifyContent='space-between' sx={{ color: '#FFF'}} py={1} px={2}>
               <Link to={`/channel/${channelId}`}>
                 <Typography variant='subtitle1'color='#FFF'>
@@ -44,6 +66,7 @@ export default function VideoDetail({  }) {
               </Stack>
             </Stack>
           </Box>
+           
         </Box>
         <Box px={2} py={{md: 1, xs: 5}} justifyContent='center' alignItems='center'>
           <Typography color='#FFF' fontSize='20px' fontWeight={500} mb={1} sx={{opacity: .9}}>Related videos</Typography>
--- src/components/index.js
+++ src/components/index.js
@@ -8,4 +8,6 @@ export { default as SideBar } from './SideBar'
 export { default as Videos } from './Videos'
 export { default as VideoCard } from './VideoCard'
 export { default as ChannelCard } from './ChannelCard'
+export { default as NextVideo } from './NextVideo'
+export { default as ToggleSwitch } from './ToggleSwitch'
 
